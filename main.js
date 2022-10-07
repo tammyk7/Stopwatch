d6d7094eb39960594e387efdb34e0c6dcaa06165
Tammy <tammy_khan786@hotmail.co.uk>
2022-10-07 13:18:51 +0100

    indented all files

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ const $lapTable = document.getElementById('lapTable')
 const lapTimer = document.getElementById('lapTimer')
 
 const stopwatch = { 
-    elapsedTime: 0,
-    lapElapsedTime:0,
-    startTime: 0,
-    lapTime: 0,
-    intervalId: 0 
+elapsedTime: 0,
+lapElapsedTime:0,
+startTime: 0,
+lapTime: 0,
+intervalId: 0 
 }
 
 let isStarted = false
@@ -23,49 +23,49 @@ let lapRow
 const padNumber =  (value) => Math.floor(value).toString().padStart(2, '0')
 
 const getFormattedTime = (elapsedTime) => {
-    const centiseconds =(elapsedTime % 1000) / 10
-    const seconds = (elapsedTime / 1000) % 60
-    const minutes =(elapsedTime / (1000 * 60)) % 60
-    return [centiseconds, seconds, minutes].map(padNumber) 
+const centiseconds =(elapsedTime % 1000) / 10
+const seconds = (elapsedTime / 1000) % 60
+const minutes =(elapsedTime / (1000 * 60)) % 60
+return [centiseconds, seconds, minutes].map(padNumber) 
 }
 
 const runTimer = () => {
-    startStopButton.innerText = 'Stop'
-    lapResetButton.innerText = 'Lap'
-    isStarted = true
-    startTimer()
+startStopButton.innerText = 'Stop'
+lapResetButton.innerText = 'Lap'
+isStarted = true
+startTimer()
 }
 
 const stopTimer = () => {
-    
-    startStopButton.innerText = 'Start'
-    lapResetButton.innerText = 'Reset'
-    isStarted = false
-    clearInterval(stopwatch.intervalId)
+
+startStopButton.innerText = 'Start'
+lapResetButton.innerText = 'Reset'
+isStarted = false
+clearInterval(stopwatch.intervalId)
 }
 
 startStopButton.onclick = () =>  isStarted ? stopTimer() : runTimer() 
 
 lapResetButton.onclick = () => { 
-    if (!isStarted){
-        lapResetButton.innerText = 'Lap'
-        isStarted = false
-        resetTimer()
-    }
-    else if (lapResetButton.innerText == 'Lap'){
-        getLap()
-        compareLap()
-        stopwatch.lapTime = Date.now()
-    }
+if (!isStarted){
+    lapResetButton.innerText = 'Lap'
+    isStarted = false
+    resetTimer()
+}
+else if (lapResetButton.innerText == 'Lap'){
+    getLap()
+    compareLap()
+    stopwatch.lapTime = Date.now()
+}
 }
 
 const getLap = () => {
-    lapRow = $lapTable.insertRow(0)
-    let lapNumber = lapRow.insertCell(0)
-    let lapTime = lapRow.insertCell(1)
+lapRow = $lapTable.insertRow(0)
+let lapNumber = lapRow.insertCell(0)
+let lapTime = lapRow.insertCell(1)
 
-    lapNumber.innerText = `Lap ${lapCounter++}`
-    lapTime.innerText = ` ${formatLapTime[2]}:${formatLapTime[1]}.${formatLapTime[0]}`
+lapNumber.innerText = `Lap ${lapCounter++}`
+lapTime.innerText = ` ${formatLapTime[2]}:${formatLapTime[1]}.${formatLapTime[0]}`
 
 }
 // create a row for the lapTimer
@@ -74,39 +74,39 @@ const firstLap = () => {
 }
 
 const compareLap = () => {
-    if (stopwatch.lapElapsedTime < fastestLap) {
-        fastestLap = stopwatch.lapElapsedTime
-        // lapRow.classList.add('fastestLap')
-        lapRow.style.color = 'green'
-    }
-    if (stopwatch.lapElapsedTime > slowestLap) {
-        slowestLap = stopwatch.lapElapsedTime
-        // lapRow.classList.add('slowestLap')
-        lapRow.style.color = 'red'
-    }
+if (stopwatch.lapElapsedTime < fastestLap) {
+    fastestLap = stopwatch.lapElapsedTime
+    // lapRow.classList.add('fastestLap')
+    lapRow.style.color = 'green'
+}
+if (stopwatch.lapElapsedTime > slowestLap) {
+    slowestLap = stopwatch.lapElapsedTime
+    // lapRow.classList.add('slowestLap')
+    lapRow.style.color = 'red'
+}
 }
 
 const startTimer = () => {
-        stopwatch.startTime = Date.now() - stopwatch.elapsedTime
-        stopwatch.lapTime = Date.now() - stopwatch.lapElapsedTime
+    stopwatch.startTime = Date.now() - stopwatch.elapsedTime
+    stopwatch.lapTime = Date.now() - stopwatch.lapElapsedTime
 
-        stopwatch.intervalId = setInterval(() => { 
-        
-            stopwatch.elapsedTime = Date.now() - stopwatch.startTime 
-            stopwatch.lapElapsedTime = Date.now() - stopwatch.lapTime 
-            const formatTime = getFormattedTime(stopwatch.elapsedTime)
-            formatLapTime = getFormattedTime(stopwatch.lapElapsedTime)
+    stopwatch.intervalId = setInterval(() => { 
+    
+        stopwatch.elapsedTime = Date.now() - stopwatch.startTime 
+        stopwatch.lapElapsedTime = Date.now() - stopwatch.lapTime 
+        const formatTime = getFormattedTime(stopwatch.elapsedTime)
+        formatLapTime = getFormattedTime(stopwatch.lapElapsedTime)
 
-            timer.innerText =`${formatTime[2]}:${formatTime[1]}.${formatTime[0]}` 
-            lapTimer.innerText =`${formatLapTime[2]}:${formatLapTime[1]}.${formatLapTime[0]}`
-    }, 10)
+        timer.innerText =`${formatTime[2]}:${formatTime[1]}.${formatTime[0]}` 
+        lapTimer.innerText =`${formatLapTime[2]}:${formatLapTime[1]}.${formatLapTime[0]}`
+}, 10)
 }
 
 const resetTimer = () => {
-    stopwatch.elapsedTime = 0
-    stopwatch.lapElapsedTime = 0
-    timer.innerText = '00:00.00'
-    lapTimer.innerText = '' 
-    $lapTable.innerText = ''
-    lapCounter = 1
+stopwatch.elapsedTime = 0
+stopwatch.lapElapsedTime = 0
+timer.innerText = '00:00.00'
+lapTimer.innerText = '' 
+$lapTable.innerText = ''
+lapCounter = 1
 }
